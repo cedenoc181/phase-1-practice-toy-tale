9a036b403c0546fe416e683a2eb777d576e6ed06
Christian <cedenoc181@gmail.com>
2022-09-10 13:50:48 -0400

    commit tree 40fafbb17e1613f7fd43a30a1f86a64f8607b726

--- src/index.js
+++ src/index.js
@@ -13,3 +13,64 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+fetch(' http://localhost:3000/toys')
+.then(res => res.json())
+.then(toyData => {
+  toyData.forEach(data => 
+    renderToys(data))
+})
+
+const renderToys = (data) => {
+  //console.log(data)
+  let toyCollection = document.querySelector('#toy-collection')
+toyCollection.innerHTML = toyCollection.innerHTML + `<div class="card"> <h2>${data.name}</h2> <img class="toy-avatar" src = "${data.image}" width = "100"> <p> likes: ${data.likes} </p><button onclick="increaseLikes(${data.id}, ${data.likes})" class= "like-btn" id = "${data.id}">like</button> </div>`
+ 
+}
+function increaseLikes(id, oldLikes){
+  let newNumberOfLikes = parseInt(oldLikes) + 1
+  fetch('http://localhost:3000/toys/'+id,
+  {
+  method : "PATCH",
+  headers:
+  {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  },
+  body: JSON.stringify({
+    "likes": newNumberOfLikes
+
+  })
+}
+  )
+  window.location.reload()
+}
+
+let form = document.querySelector('#form')
+
+form.addEventListener('submit', function(event){
+  event.preventDefault()
+  
+  let imgURL = form.querySelector('input[name="image"]').value
+  let name = form.querySelector('input[name="name"]').value
+ 
+
+fetch('http://localhost:3000/toys',
+{
+method : "POST",
+headers:
+{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+},
+body: JSON.stringify({
+  name : name,
+image : imgURL,
+likes: 0
+})
+}
+
+)
+}   )
